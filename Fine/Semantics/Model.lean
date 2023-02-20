95f03d45c61e1f67dd534064aa59aeac20b8d173
Graham Leach-Krouse <gleachkr@gmail.com>
2023-02-20 12:53:30 -0600

    Improve satisfies definition, primes subtype

--- Fine/Semantics/Model.lean
+++ Fine/Semantics/Model.lean
@@ -10,7 +10,7 @@ class Model (α : Type u)
     prime : Set α
     application : α → α → α
     identity : α
-    routeleyStar : {x : α // prime x} → {x : α // prime x}
+    routeleyStar : Subtype prime → Subtype prime
     valuation : α → Set Nat
     appMonotoneLeft : ∀x : α, Monotone (application x)
     appMonotoneRight : ∀x : α, Monotone (flip application x)
@@ -20,6 +20,11 @@ class Model (α : Type u)
     valBounding : ∀t : α, ∀x : Nat, (∀p : α, prime p ∧ t ≤ p → x ∈ valuation p) → x ∈ valuation t
     starAntitone : Antitone routeleyStar
     starInvolution: Function.Involutive routeleyStar
+
+def Model.primes [m : Model α] : Type := Subtype m.prime
+
+instance primesToSubtype [m : Model α] : Coe m.primes α where
+  coe x := x.val
 
 infix:256 "∘" => Model.application --overloading composition symbol here
 postfix:256 "*" => Model.routeleyStar
--- Fine/Semantics/Satisfaction.lean
+++ Fine/Semantics/Satisfaction.lean
@@ -4,32 +4,32 @@ import Fine.PropositionalLanguage
 def satisfies [inst : Model α] (t : α) (f : Form) : Prop :=
   match f with
     | #n => n ∈ inst.valuation t
-    | ~f => ∀{p : α}, ∀h : p ∈ inst.prime, t ≤ p → ¬(satisfies (⟨p,h⟩*).val f)
+    | ~f => ∀{p : inst.primes}, t ≤ p → ¬(satisfies (p*).val f)
     | f & g => satisfies t f ∧ satisfies t g
-    | f ¦ g => ∀{p : α}, ∀h : p ∈ inst.prime, t ≤ p → satisfies (⟨p,h⟩*).val f ∨ satisfies (⟨p,h⟩*).val g
+    | f ¦ g => ∀{p : inst.primes}, t ≤ p → satisfies (p*).val f ∨ satisfies (p*).val g
     | Form.impl f g => ∀{u : α}, satisfies u f → satisfies (t ∘ u) g
     --can't pattern match using ⊃ because of collision with set
 
 infix:128 "⊨"  => satisfies
 
-theorem satisfactionUp [inst : Model α] {s t : α} {f : Form} (h₁: s ≤ t) (h₂ : s ⊨ f) : t ⊨ f := 
+theorem upwardsClosure [inst : Model α] {s t : α} {f : Form} (h₁: s ≤ t) (h₂ : s ⊨ f) : t ⊨ f := 
   match f with
     | #n => have l₁ : inst.valuation s ⊆ inst.valuation t := inst.valMonotone s t h₁
             l₁ h₂
     | ~g => by unfold satisfies
-               intros p h₃ h₄ h₅
-               exact h₂ h₃ (le_trans' h₄ h₁) h₅
+               intros p h₃ h₄
+               exact h₂ (le_trans h₁ h₃) h₄
     | f & g => by unfold satisfies
                   apply And.intro
-                  case left => exact satisfactionUp h₁ h₂.left
-                  case right => exact satisfactionUp h₁ h₂.right
+                  case left => exact upwardsClosure h₁ h₂.left
+                  case right => exact upwardsClosure h₁ h₂.right
     | f ¦ g => by unfold satisfies
-                  intros p h₃ h₄
-                  apply Or.elim (h₂ h₃ (le_trans' h₄ h₁))
-                  case left => intro h₅; exact Or.inl h₅
-                  case right => intro h₅; exact Or.inr h₅
+                  intros p h₃
+                  apply Or.elim (h₂ (le_trans' h₃ h₁))
+                  case left => intro h₄; exact Or.inl h₄
+                  case right => intro h₄; exact Or.inr h₄
     | Form.impl f g => by unfold satisfies
                           intros u h₃
-                          let l₁ : s ∘ u ≤ t ∘ u := inst.appMonotoneRight u h₁
-                          let l₂ : (s ∘ u) ⊨ g := h₂ h₃
-                          exact satisfactionUp l₁ l₂
+                          have l₁ : s ∘ u ≤ t ∘ u := inst.appMonotoneRight u h₁
+                          have l₂ : (s ∘ u) ⊨ g := h₂ h₃
+                          exact upwardsClosure l₁ l₂
