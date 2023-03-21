26f4425224e7b545a9ef485af7accf3421aec1cc
gleachkr@gmail.com <gleachkr@gmail.com>
2023-03-21 14:26:19 -0500

    Strengthen compactness lemma

--- Fine/Hilbert/SystemB.lean
+++ Fine/Hilbert/SystemB.lean
@@ -48,27 +48,39 @@ def BProof.adjoinPremises { r : Form } : BProof {p,q} r → BProof {p & q} r
   | mp h₁ h₂ => mp (adjoinPremises h₁) h₂
   | adj h₁ h₂ => adj (adjoinPremises h₁) (adjoinPremises h₂)
 
-theorem BProof.compactness { Γ : Ctx } { f : Form } : BProof Γ f → Σs : Finset Form, BProof ↑s f := by
+theorem BProof.compactness { Γ : Ctx } { f : Form } : BProof Γ f → Σs : Finset Form, Σ'_ : ↑s ⊆ Γ,  BProof ↑s f := by
   intros prf₁; induction prf₁
-  case ax g _ => 
+  case ax g h₁ => 
+    let Gsing : Finset Form := {g} 
     have l₁ : g ∈ {g} := Finset.mem_singleton.mpr rfl
-    have l₂ : ({g} : Finset Form) = ({g} : Ctx) := Finset.coe_singleton g
+    have l₂ : Gsing = ({g} : Ctx) := Finset.coe_singleton g
+    have l₃ : ↑Gsing ⊆ Γ := by
+      intros g' h₂
+      rw [l₂] at h₂
+      rw [h₂]
+      assumption
     have prf₂ : BProof ↑{g} g := by
       rw [←l₂]
       apply ax l₁
     rw [←l₂] at prf₂
-    exact ⟨↑{g}, prf₂⟩
+    exact ⟨Gsing, l₃, prf₂⟩
   case mp P Q _ h₂ ih => 
-    have ⟨fin, prf⟩ := ih
-    exact ⟨fin, mp prf h₂⟩
+    have ⟨fin, h₁, prf⟩ := ih
+    exact ⟨fin, h₁, mp prf h₂⟩
   case adj P Q _ _ ih₁ ih₂ => 
-    have ⟨fin₁, prf₁⟩ := ih₁
-    have ⟨fin₂, prf₂⟩ := ih₂
+    have ⟨fin₁, h₁, prf₁⟩ := ih₁
+    have ⟨fin₂, h₂, prf₂⟩ := ih₂
     have prf₃ : BProof (↑fin₁ ∪ ↑fin₂) P := BProof.monotone (Set.subset_union_left ↑fin₁ ↑fin₂) prf₁
     have prf₄ : BProof (↑fin₁ ∪ ↑fin₂) Q := BProof.monotone (Set.subset_union_right ↑fin₁ ↑fin₂) prf₂
     have prf₅ := adj prf₃ prf₄
+    have l₁ : ↑(fin₁ ∪ fin₂) ⊆ Γ := by
+      intros f h₃
+      rw [Finset.coe_union] at h₃
+      cases h₃
+      case inl h₄ => exact h₁ h₄
+      case inr h₄ => exact h₂ h₄
     rw [←Finset.coe_union] at prf₅
-    exact ⟨↑(fin₁ ∪ fin₂), prf₅⟩
+    exact ⟨↑(fin₁ ∪ fin₂), l₁, prf₅⟩
 
 def BTheorem.transitivity (h₁ : BTheorem (p ⊃ q)) (h₂ : BTheorem (q ⊃ r)) : BTheorem (p ⊃ r) :=
   mp taut (hs h₁ h₂) 
