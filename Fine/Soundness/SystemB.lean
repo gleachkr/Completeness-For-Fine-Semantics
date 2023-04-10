15c20283391a79f8558d0698527a01abadb47ef1
Graham Leach-Krouse <gleachkr@gmail.com>
2023-04-09 21:53:51 -0500

    Tidy soundness proof

--- Fine/Soundness/SystemB.lean
+++ Fine/Soundness/SystemB.lean
@@ -11,94 +11,68 @@ def systemBSoundness (prf: BTheorem f) : valid f := by
   case andE₁ => unfold satisfies at h₁; apply And.left; assumption
   case andE₂ => unfold satisfies at h₁; apply And.right; assumption
   case andI => 
-    unfold satisfies at h₁
-    unfold satisfies at h₁
-    unfold satisfies
     intro u h₂
     apply And.intro
     case left => exact h₁.left h₂
     case right => exact h₁.right h₂
   case orI₁ =>
-    unfold satisfies
     intros p h₂
     apply Or.inl
     exact upwardsClosure h₂ h₁
   case orI₂ =>
-    unfold satisfies
     intros p h₂
     apply Or.inr
     exact upwardsClosure h₂ h₁
-  case orE =>
-    unfold satisfies
+  case orE P Q R=>
     intro u h₂
-    unfold satisfies at h₂
     apply primeDetermination
     intros p h₃
-    rename_i P Q R
     have ⟨_, r, _, c₁,_,c₂⟩ : ∃q r : M.primes, x ≤ q ∧ u ≤ r ∧ (↑q ∙ u) ≤ p ∧ (x ∙ r) ≤ p := M.appBounding x u p h₃
     have l₂ : r ⊨ P ∨ r ⊨ Q := h₂ c₁
     have l₃ : (x ∙ r) ⊨ R := by 
       cases l₂
-      case inl => rename_i h₄; exact h₁.1 h₄
-      case inr => rename_i h₄; exact h₁.2 h₄
+      case inl h₄ => exact h₁.1 h₄
+      case inr h₄ => exact h₁.2 h₄
     exact upwardsClosure c₂ l₃
-  case dne => 
+  case dne P => 
     apply primeDetermination
     intros p h₂
-    unfold satisfies at h₁
-    unfold satisfies at h₁
-    rename_i P
     have ⟨q, l₁⟩ : ∃q : M.primes, ¬((p*).val ≤ q.val → ¬((q*).val) ⊨ P) := nonconstruction (h₁ h₂)
     have ⟨l₂, l₃⟩ := nonconstruction l₁
     have l₄ := M.starAntitone l₂
     rw [M.starInvolution] at l₄
     exact upwardsClosure l₄ (byContradiction l₃)
   case dist => 
-    unfold satisfies
     intros p h₂
-    unfold satisfies at h₁
     have ⟨l₁,l₂⟩ := h₁
-    unfold satisfies at l₂
     cases l₂ h₂
-    case inl h₃ => 
-      apply Or.inl
-      unfold satisfies
-      exact ⟨upwardsClosure h₂ l₁, h₃⟩
-    case inr h₃ => 
-      apply Or.inr
-      unfold satisfies
-      exact ⟨upwardsClosure h₂ l₁, h₃⟩
-  case adj thm₁ thm₂ => 
-    unfold satisfies
-    exact ⟨thm₁ α, thm₂ α⟩
-  case mp thm₁ thm₂ => 
-    rename_i P Q R S
-    have l₁ : M.identity ⊨ P := thm₁ α
-    have l₃ : M.identity ⊨ P ⊃ Q := thm₂ α
+    case inl h₃ => exact Or.inl ⟨upwardsClosure h₂ l₁, h₃⟩
+    case inr h₃ => exact Or.inr ⟨upwardsClosure h₂ l₁, h₃⟩
+  case adj val₁ val₂ => exact ⟨val₁ α, val₂ α⟩
+  case mp P Q _ _ val₁ val₂ => 
+    have l₁ : M.identity ⊨ P := val₁ α
+    have l₃ : M.identity ⊨ P ⊃ Q := val₂ α
     have l₄ := l₃ l₁
     rw [←M.appLeftIdent M.identity]
     assumption
-  case cp thm₁ => 
-    rename_i P Q _
+  case cp P Q _ val₁ => 
     apply primeDetermination
     apply byContradiction
     intros h₂
     have ⟨q, l₂⟩ := nonconstruction h₂
     have ⟨l₃, l₄⟩ := nonconstruction l₂
     have l₅ := starCompatLeft l₄
-    have l₆ : M.identity ⊨ P ⊃ ~Q:= thm₁ α
+    have l₆ : M.identity ⊨ P ⊃ ~Q:= val₁ α
     have l₇ : (M.identity ∙ (q*).val) ⊨ ~Q := l₆ l₅
-    unfold satisfies at l₇
     have l₈ : ¬(q** ⊨ Q) := by
       rw [M.appLeftIdent (q*).val] at l₇
       exact l₇ (le_refl (q*).val)
     rw [M.starInvolution q] at l₈
     exact l₈ $ upwardsClosure l₃ h₁
-  case hs thm₁ thm₂ => 
-    unfold satisfies
+  case hs val₁ val₂ => 
     intros u h₂
-    have l₁ := (thm₁ α) h₂
+    have l₁ := (val₁ α) h₂
     rw [M.appLeftIdent u] at l₁
-    have l₂ := (thm₂ α) (h₁ l₁)
+    have l₂ := (val₂ α) (h₁ l₁)
     rw [M.appLeftIdent (x ∙ u)] at l₂
     assumption
